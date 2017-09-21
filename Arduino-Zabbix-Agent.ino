f801df44237cce37bf98d8979525f94e2eb4996d
Ramiro Batista da Luz <ramiroluz@gmail.com>
2017-09-21 17:22:18 -0300

    Change to work with DS18B20 sensor.

--- Arduino-Zabbix-Agent.ino
+++ Arduino-Zabbix-Agent.ino
@@ -1,20 +1,19 @@
 /******************************************************************************
- * Purpose: Provide a way Zabbix access Temperature and Humidity.
- * Git: https://github.com/devlware/Arduino-Zabbix-Agent
- * Author: Diego W. Antunes <devlware@gmail.com>
- * Adapted from: Gabriel Ferreira and Marco Rougeth and Marco Fischer
- *
- * Circuit:
- *  - Used Arduino Ethernet or
- *  - Arduino Uno + Ethernet shield uses pins 4, 10, 11, 12, 13
- *  - DHT22 sensor use pin 5, VCC and GND
- * 
- * Required external libraries:
- *   - DHT Sensors https://github.com/adafruit/DHT-sensor-library
+   Purpose: Provide a way Zabbix access Temperature and Humidity.
+   Git: https://github.com/ramiroluz/Arduino-Zabbix-Agent
+   Author: Ramiro Batista da Luz <ramiroluz@gmail.com>
+   Adapted from Diego W. Antunes, Gabriel Ferreira, Marco Rougeth and Marco Fischer
+
+   Circuit:
+    - Arduino Uno + Ethernet shield uses pins 4, 10, 11, 12, 13
+    - DS18B20 DHT22 sensor use pin 2, VCC and GND
+
+   Required external libraries:
+     - DallasTemperature Arduino Library https://www.milesburton.com/Dallas_Temperature_Control_Library
  *****************************************************************************/
 
-#include <DHT.h>
-#include <DHT_U.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <Ethernet.h>
 #include <SPI.h>
 #include <string.h>
@@ -26,259 +25,244 @@
 #endif
 
 #ifndef DHCP_ON
-#define DHCP_ON
+//#define DHCP_ON
 #endif
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x5D, 0x05 };
 IPAddress ip_host(192, 168, 2, 2);
-IPAddress ip_gw(192, 168, 2, 254);
+IPAddress ip_gw(192, 168, 2, 0);
 IPAddress subnet(255, 255, 255, 0);
 EthernetServer g_server(10050);
 EthernetClient g_client;
 
-#define DHT_PIN            5         // Pin which is connected to the DHT sensor
-//#define DHT_TYPE         DHT11     // DHT 11
-//#define DHT_TYPE         DHT21     // DHT 21 (AM2301)
-#define DHT_TYPE           DHT22     // DHT 22 (AM2302)
-
-DHT_Unified dht(DHT_PIN, DHT_TYPE);
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 2
 
 boolean cli_connected = false;  // Controll if client is connected
 double dht_temp = 0;            // Temperature (Celsius)
-double dht_umid = 0;            // Humidity (%)
 String cmd;                     // Zabbix command
 int8_t eth_maintain = -1;       // Ethernet dhcp variable
 const uint8_t MAX_CMD_LENGTH = 25; // Max command message
-const uint16_t DHT_READ_TIME = 10000; // Time between readings (milliseconds)
+const uint16_t READ_TIME = 10000; // Time between readings (milliseconds)
 uint16_t wdt_state = 0x0000;    // Watchdog control
-unsigned long dht_last_check = 0; // Controls last read time
+unsigned long last_check = 0; // Controls last read time
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device address
+DeviceAddress insideThermometer;
+
+// Store last sensor temperature.
+float tempC;
+
 
 /************************************************************************************
- * Description: Setup function, config MAC, IP, initiate server and watchdog.
- * Notes:
+   Description: Setup function, config MAC, IP, initiate server and watchdog.
+   Notes:
  ************************************************************************************/
 void setup()
 {
+  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0");
+  sensors.getResolution(insideThermometer);
+  
 #ifdef SERIAL_DEBUG
-    Serial.begin(9600);
-    Serial.println("Setup");
+  Serial.begin(9600);
+  Serial.println("Setup");
 #endif
 
-    // Set watchdog to 8 seconds.
-    watchdog_setup();
+  // Set watchdog to 8 seconds.
+  watchdog_setup();
 
 #ifdef DHCP_ON
-    int dhcp_return = 0;
-    const uint8_t max_num_dhcp_tries = 5;
-    uint8_t num_dhcp_tries = 0;
-    bool f_dhcp_done = false;
+  int dhcp_return = 0;
+  const uint8_t max_num_dhcp_tries = 5;
+  uint8_t num_dhcp_tries = 0;
+  bool f_dhcp_done = false;
 
-    do {
-        dhcp_return = Ethernet.begin(mac);
+  do {
+    dhcp_return = Ethernet.begin(mac);
 
-        if (dhcp_return == 1) {
-            f_dhcp_done = true;
+    if (dhcp_return == 1) {
+      f_dhcp_done = true;
 #ifdef SERIAL_DEBUG
-            Serial.println(Ethernet.localIP());
+      Serial.println(Ethernet.localIP());
 #endif
-        } else {
-            // Too bad, DHCP server did not provided a valid IP address.
-            delay(5000);
+    } else {
+      // Too bad, DHCP server did not provided a valid IP address.
+      delay(5000);
 #ifdef SERIAL_DEBUG
-            Serial.println(num_dhcp_tries);
+      Serial.println(num_dhcp_tries);
 #endif
-        }
-
-        num_dhcp_tries++;
-        wdt_reset();
-    } while ((num_dhcp_tries < 5) && (f_dhcp_done == false));
-
-    // If could not get a IP from dhcp server reset.
-    if ((num_dhcp_tries >= 5) && (f_dhcp_done == false)) {
-        halt();
     }
 
+    num_dhcp_tries++;
+    wdt_reset();
+  } while ((num_dhcp_tries < 5) && (f_dhcp_done == false));
+
+  // If could not get a IP from dhcp server reset.
+  if ((num_dhcp_tries >= 5) && (f_dhcp_done == false)) {
+    halt();
+  }
+
 #else
-    Ethernet.begin(mac, ip_host, ip_gw, subnet);
+  Ethernet.begin(mac, ip_host, ip_gw, subnet);
 #endif
 
-    g_server.begin();
+  g_server.begin();
 }
 
 /************************************************************************************
- * Description: Read data, check WDT and manage client connection.
- * Notes:
+   Description: Read data, check WDT and manage client connection.
+   Notes:
  ************************************************************************************/
 void loop()
 {
-    wdt_state = 0x5555;
-    wdt_a();
+  wdt_state = 0x5555;
+  wdt_a();
 
-    dht_get_data();
-    g_client = g_server.available();
+  get_data(insideThermometer);
+  g_client = g_server.available();
 
 #ifdef DHCP_ON
-    eth_maintain = Ethernet.maintain();
+  eth_maintain = Ethernet.maintain();
 #endif
 
-    if (g_client) {
-        if (!cli_connected) {
-            cli_connected = true;
-        }
-
-        int to_read = g_client.available();
-        if (to_read > 0) {
-            read_stream(g_client.read());
-        }
+  if (g_client) {
+    if (!cli_connected) {
+      cli_connected = true;
     }
 
-    delay(10);
-    wdt_state += 0x2222;
-    wdt_b();
+    int to_read = g_client.available();
+    if (to_read > 0) {
+      read_stream(g_client.read());
+    }
+  }
+
+
+  delay(10);
+  wdt_state += 0x2222;
+  wdt_b();
 }
 
 /************************************************************************************
- * Description: Read received command.
- * Notes:
+   Description: Read received command.
+   Notes:
  ************************************************************************************/
 static void read_stream(const char c)
 {
-    if (cmd.length() == MAX_CMD_LENGTH) {
-       cmd = "";
-    }
+  if (cmd.length() == MAX_CMD_LENGTH) {
+    cmd = "";
+  }
 
-    cmd += c;
+  cmd += c;
 
-    if (c == 10 || c == 13) {
-        if (cmd.length() >= 2) {
+  if (c == 10 || c == 13) {
+    if (cmd.length() >= 2) {
 #ifdef SERIAL_DEBUG
-            for (int i = 0; i < cmd.length(); ++i) {
-                g_server.println(cmd.charAt(i), DEC);
-            }
+      for (int i = 0; i < cmd.length(); ++i) {
+        g_server.println(cmd.charAt(i), DEC);
+      }
 #endif
-            cmd = cmd.substring(0, cmd.length() - 1);
-            parse_command();
-        }
-        cmd = "";
+      cmd = cmd.substring(0, cmd.length() - 1);
+      parse_command();
     }
+    cmd = "";
+  }
 }
 
 /************************************************************************************
- * Description: Parse received command.
- * Notes:
+   Description: Parse received command.
+   Notes:
  ************************************************************************************/
 void parse_command(void)
 {
 #ifdef SERIAL_DEBUG
-    g_server.println(cmd);
-    g_server.println(cmd.length());
+  g_server.println(cmd);
+  g_server.println(cmd.length());
 #endif
 
-    if (cmd.equals("agent.ping")) {
-        g_server.println("pong");
+  if (cmd.equals("agent.ping")) {
+    g_server.println("pong");
 #ifdef SERIAL_DEBUG
-        Serial.println("pong");
-#endif     
-    } else if (cmd.equals("agent.version")) {
-        g_server.println("Arduino Zabbix Agent 0.1");
-#ifdef SERIAL_DEBUG
-        Serial.println("Arduino Zabbix Agent 0.1");
+    Serial.println("pong");
 #endif
-    } else if (cmd.equals("agent.temp")) {
-        g_server.println(dht_temp);
+  } else if (cmd.equals("agent.version")) {
+    g_server.println("Arduino Zabbix Agent 0.1");
 #ifdef SERIAL_DEBUG
-        Serial.println(dht_temp);
-#endif     
-    } else if (cmd.equals("agent.umid")) {
-        g_server.println(dht_umid);
-#ifdef SERIAL_DEBUG
-        Serial.println(dht_umid);
+    Serial.println("Arduino Zabbix Agent 0.1");
 #endif
-    } else if (cmd.equals("agent.ethernet")) {
-        g_server.println(eth_maintain);
+  } else if (cmd.equals("agent.temp")) {
+    g_server.println(tempC);
 #ifdef SERIAL_DEBUG
-        Serial.println(eth_maintain);
+    Serial.println(tempC);
 #endif
-    } else if (cmd.equals("agent.close")) {
-        g_server.println("closed");
+  } else if (cmd.equals("agent.ethernet")) {
+    g_server.println(eth_maintain);
 #ifdef SERIAL_DEBUG
-        Serial.println("closed");
-#endif     
-        g_client.stop();
-        cli_connected = false;     
-    } else { // Agent error
-        g_server.println("Invalid command");
-    }
+    Serial.println(eth_maintain);
+#endif
+  } else if (cmd.equals("agent.close")) {
+    g_server.println("closed");
+#ifdef SERIAL_DEBUG
+    Serial.println("closed");
+#endif
+    g_client.stop();
+    cli_connected = false;
+  } else { // Agent error
+    g_server.println("Invalid command");
+  }
 
     // Zabbix prefers that the connection is closed after each command.
     // FIXME, needs more testing.
     g_client.stop();
-    cli_connected = false;
+  cli_connected = false;
 }
 
 /************************************************************************************
- * Description: Read DHT sensor data.
- * Notes:
+   Description: Read sensor data.
+   Notes:
  ************************************************************************************/
-void dht_get_data()
+float get_data(DeviceAddress deviceAddress)
 {
-    if (millis() - dht_last_check > DHT_READ_TIME) {
-//        Used to test when there is no sensor connected, need to disable lines below.
-//        dht_temp = 77.66;
-//        dht_umid = 60.66;
+  if (millis() - last_check > READ_TIME) {
 #ifdef SERIAL_DEBUG
-        Serial.println("read new sensor data");
+    Serial.println("read new sensor data");
 #endif
-        sensors_event_t event;
-        dht.temperature().getEvent(&event);
+    sensors.requestTemperatures();
+    tempC = sensors.getTempC(deviceAddress);
 
-        if (isnan(event.temperature)) {
+    if (isnan(tempC)) {
 #ifdef SERIAL_DEBUG
-            Serial.println("Error reading temperature!");
+      Serial.println("Error reading temperature!");
 #endif
-        } else {
-            dht_temp = event.temperature;
-            //g_server.println(dht_temp);
+    } else {
 #ifdef SERIAL_DEBUG
-            Serial.print("Temperature: ");
-            Serial.print(dht_temp);
-            Serial.println(" *C");
+      Serial.print("Temperature: ");
+      Serial.print(tempC);
+      Serial.println(" *C");
 #endif
-        }
-
-        // Get humidity event and print its value.
-        dht.humidity().getEvent(&event);
-
-        if (isnan(event.relative_humidity)) {
-#ifdef SERIAL_DEBUG
-            Serial.println("Error reading humidity!");
-#endif
-        } else {
-            dht_umid = event.relative_humidity;
-            //g_server.println(dht_umid);
-#ifdef SERIAL_DEBUG
-            Serial.print("Humidity: ");
-            Serial.print(dht_umid);
-            Serial.println("%");
-#endif
-        }
-
-        dht_last_check = millis();
     }
+
+    last_check = millis();
+  }
 }
 
 /************************************************************************************
- * Description: Configure watchdog, enable interrupts and system reset.
- * Notes: Watchdog is set to 8 seconds.
+   Description: Configure watchdog, enable interrupts and system reset.
+   Notes: Watchdog is set to 8 seconds.
  ************************************************************************************/
 void watchdog_setup(void)
 {
-    // Disable all interrupts.
-    cli();
+  // Disable all interrupts.
+  cli();
 
-    // Reset watchdog timer.
-    wdt_reset();
-    /*
+  // Reset watchdog timer.
+  wdt_reset();
+  /*
     WDTCSR configuration:
     WDIE = 1 :Interrupt Enable
     WDE  = 1 :Reset Enable
@@ -287,54 +271,54 @@ void watchdog_setup(void)
     WDP2 = 0 :For 8000ms Time-out
     WDP1 = 0 :For 8000ms Time-out
     WDP0 = 1 :For 8000ms Time-out
-    */
-    // Configuro os bits para poder modificar a configuracao de tempo do watchdog
-    WDTCSR |= (1 << WDCE) | (1 << WDE);
-    // Setup watchdog bits
-    WDTCSR = (1 << WDIE) | (1 << WDE) |(1 << WDP3) | (0 << WDP2) | (0 << WDP1) | (1 << WDP0);
+  */
+  // Configuro os bits para poder modificar a configuracao de tempo do watchdog
+  WDTCSR |= (1 << WDCE) | (1 << WDE);
+  // Setup watchdog bits
+  WDTCSR = (1 << WDIE) | (1 << WDE) | (1 << WDP3) | (0 << WDP2) | (0 << WDP1) | (1 << WDP0);
 
-    // Reenable system interrupts.
-    sei();
+  // Reenable system interrupts.
+  sei();
 }
 
 /************************************************************************************
- * Description: Check that system state is still consistent.
- * Notes: If state condition is not ok, halts system.
+   Description: Check that system state is still consistent.
+   Notes: If state condition is not ok, halts system.
  ************************************************************************************/
 void wdt_a(void)
 {
-    if (wdt_state != 0x5555)
-        halt();
+  if (wdt_state != 0x5555)
+    halt();
 
-    // Pat the dog otherwise it can bite.
-    wdt_reset();
-    wdt_state += 0x1111;
+  // Pat the dog otherwise it can bite.
+  wdt_reset();
+  wdt_state += 0x1111;
 }
 
 /************************************************************************************
- * Description: Check that system state is still consistent.
- * Notes: If state condition is not ok, halts system.
+   Description: Check that system state is still consistent.
+   Notes: If state condition is not ok, halts system.
  ************************************************************************************/
 void wdt_b(void)
 {
-    if (wdt_state != 0x8888)
-        halt();
+  if (wdt_state != 0x8888)
+    halt();
 
-    // Pat the dog otherwise it can bite.
-    wdt_reset();
+  // Pat the dog otherwise it can bite.
+  wdt_reset();
 
-    if (wdt_state != 0x8888)
-        halt();
+  if (wdt_state != 0x8888)
+    halt();
 
-    wdt_state = 0;
+  wdt_state = 0;
 }
 
 /************************************************************************************
- * Description: Function called when something goes wrong with watchdog.
- * Notes:
+   Description: Function called when something goes wrong with watchdog.
+   Notes:
  ************************************************************************************/
 void halt(void)
 {
-    // Stays here waiting system to reset to avoid more trouble.
-    while (1);
+  // Stays here waiting system to reset to avoid more trouble.
+  while (1);
 }
